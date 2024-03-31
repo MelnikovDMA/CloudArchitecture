4e8117b91350c55e267fd26c3b92a2596c0b78cc
Jesass351 <mikanev03@mail.ru>
2024-03-31 12:11:55 +0300

    init

--- app/static/AuthForm.js
+++ app/static/AuthForm.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-    const login_btn = document.getElementById('login_btn');
-    const register_btn = document.getElementById('register_btn');
+    const loginBtn = document.getElementById('login_btn');
+    const registerBtn = document.getElementById('register_btn');
     const form = document.getElementById('form');
 }
 
-login_btn.onclick = (event) => {
+loginBtn.onclick = (event) => {
     form.action = event.target.dataset.endpoint;
     console.log(form.action);
 }
 
-register_btn.onclick = (event) => {
+registerBtn.onclick = (event) => {
     form.action = event.target.dataset.endpoint
 }
 
