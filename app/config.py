f98c2c63aa4d516d849f435d3c9ae70f17e0db6f
MelnikovDMA <starwarriordm@gmail.com>
2024-04-18 16:45:14 +0300

    authPage

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 SECRET_KEY = 'kdnndkkw'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:cloud@mysql_db:3306/cloud'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:melnikov@mysql_db:3306/melnikov'
 #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2033_cloud:123456789@std-mysql.ist.mospolytech.ru/std_2033_cloud'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
